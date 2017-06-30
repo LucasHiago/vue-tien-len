c14b6d17b82baf23f8083bec62578071248d68b5
Jason Dinh <jasondinh@Jasons-MacBook-Pro.local>
2017-06-29 20:13:12 -0400

    designated who goes first

--- src/components/main.vue
+++ src/components/main.vue
@@ -4,38 +4,52 @@
       <button @click="play()">Play</button>
     </div>
     <div>
-
-      <template v-if="gameStart">
-        <h3>Player 1 <button @click="sortByRank('player1')">Sort by rank</button></h3>
+  
+      <template v-if="gameState.gameStart">
+        <!-- active hands area -->
+  
+        <h3>Player 1
+          <button @click="sortByRank('player1')">Sort by rank</button>
+          <button @click="submitHand('player1')" :disabled="!gameState.player1.isTurn">Play hand</button>
+        </h3>
         <div class="hand">
-          <div v-for="(card, index) in player1.cards" :key="card" class="card-container">
+          <div v-for="(card, index) in gameState.player1.cards" :key="card" class="card-container">
             <div @click="cardClickHandler('player1', index)">
               <card :cardMapKey="parseInt(card.cardKey)" :is-selected="card.isSelected"></card>
             </div>
           </div>
         </div>
   
-        <h3>Player 2 <button @click="sortByRank('player2')">Sort by rank</button></h3>
+        <h3>Player 2
+          <button @click="sortByRank('player2')">Sort by rank</button>
+          <button @click="submitHand('player2')" :disabled="!gameState.player2.isTurn">Play hand</button>
+        </h3>
         <div class="hand">
-          <div v-for="card in player2.cards" :key="card" class="card-container">
+          <div v-for="card in gameState.player2.cards" :key="card" class="card-container">
             <div @click="cardClickHandler('player2', index)">
               <card :cardMapKey="parseInt(card.cardKey)" :is-selected="card.isSelected"></card>
             </div>
           </div>
         </div>
   
-        <h3>Player 3 <button @click="sortByRank('player3')">Sort by rank</button></h3>
+        <h3>Player 3
+          <button @click="sortByRank('player3')">Sort by rank</button>
+          <button @click="submitHand('player3')" :disabled="!gameState.player3.isTurn">Play hand</button>
+        </h3>
         <div class="hand">
-          <div v-for="card in player3.cards" :key="card" class="card-container">
+          <div v-for="card in gameState.player3.cards" :key="card" class="card-container">
             <div @click="cardClickHandler('player3', index)">
               <card :cardMapKey="parseInt(card.cardKey)" :is-selected="card.isSelected"></card>
             </div>
           </div>
         </div>
   
-        <h3>Player 4 <button @click="sortByRank('player4')">Sort by rank</button></h3>
+        <h3>Player 4
+          <button @click="sortByRank('player4')">Sort by rank</button>
+          <button @click="submitHand('player4')" :disabled="!gameState.player4.isTurn">Play hand</button>
+        </h3>
         <div class="hand">
-          <div v-for="card in player4.cards" :key="card" class="card-container">
+          <div v-for="card in gameState.player4.cards" :key="card" class="card-container">
             <div @click="cardClickHandler('player4', index)">
               <card :cardMapKey="parseInt(card.cardKey)" :is-selected="card.isSelected"></card>
             </div>
@@ -58,19 +72,33 @@ export default {
 
   data() {
     return {
-      player1: {
-        cards: []
-      },
-      player2: {
-        cards: []
-      },
-      player3: {
-        cards: []
-      },
-      player4: {
-        cards: []
-      },
-      gameStart: false
+      gameState: {
+        player1: {
+          cards: [],
+          isTurn: false,
+          selectedHand: [],
+          canPlayHand: false // result of evaulating selectedHand
+        },
+        player2: {
+          cards: [],
+          isTurn: false,
+          selectedHand: [],
+          canPlayHand: false // result of evaulating selectedHand
+        },
+        player3: {
+          cards: [],
+          isTurn: false,
+          selectedHand: [],
+          canPlayHand: false // result of evaulating selectedHand
+        },
+        player4: {
+          cards: [],
+          isTurn: false,
+          selectedHand: [],
+          canPlayHand: false // result of evaulating selectedHand
+        },
+        gameStart: false
+      }
     };
   },
 
@@ -79,30 +107,48 @@ export default {
       this.initializeGame();
     },
     initializeGame() {
-      this.gameStart = true;
+      this.gameState.gameStart = true;
 
       // generate deck
       const deck = new Deck();
 
       // deal cards to players
       this.dealCards(deck);
+
+      // determine who goes first
+      this.setFirstTurnPlayer();
     },
     dealCards(deck) {
-      this.player1.cards = deck.deck.slice(0, 13);
-      this.player2.cards = deck.deck.slice(13, 26);
-      this.player3.cards = deck.deck.slice(26, 39);
-      this.player4.cards = deck.deck.slice(39, 52);
+      this.gameState.player1.cards = deck.deck.slice(0, 13);
+      this.gameState.player2.cards = deck.deck.slice(13, 26);
+      this.gameState.player3.cards = deck.deck.slice(26, 39);
+      this.gameState.player4.cards = deck.deck.slice(39, 52);
+    },
+    setFirstTurnPlayer() {
+      // player with 3S goes first
+      const targetRank = 1; // 3S
+
+      let playerFound = null;
+
+      Object.keys(this.gameState).forEach((player) => {
+        if (_.find(this.gameState[player].cards, ['rank', targetRank])) {
+          playerFound = player;
+        }
+      });
+
+      this.gameState[playerFound].isTurn = true;
     },
     sortByRank(player) {
       // sort the player's hand
-      let hand = this[player].cards;
+      let hand = this.gameState[player].cards;
       // sort by rank number
       hand = _.sortBy(hand, card => parseInt(card.rank, 10));
-      this[player].cards = hand;
+      this.gameState[player].cards = hand;
     },
     cardClickHandler(player, index) {
       // set isSelected prop on card
-      this[player].cards[index].isSelected = !this[player].cards[index].isSelected;
+      // eslint-disable-next-line
+      this.gameState[player].cards[index].isSelected = !this.gameState[player].cards[index].isSelected;
     }
   },
 
