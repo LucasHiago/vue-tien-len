b70a43b2b60a250b3ee7372194bc62254a03c604
jasondinh <dinhtq1@gmail.com>
2018-09-29 11:10:43 -0500

    changed BG color to green

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,10 @@ export default {
 </script>
 
 <style>
+html, body {
+  background-color: #45A173;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
