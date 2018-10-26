5a3c45202daa54a553bdb33410729ccd04e35ca6
jasondinh <dinhtq1@gmail.com>
2018-10-26 18:44:17 -0400

    testin vue-circle comp

--- src/components/main.vue
+++ src/components/main.vue
@@ -6,6 +6,22 @@
       </template>
       <template v-else>
         <button @click="play()">Play</button>
+        <vue-circle
+          :progress="50"
+          :size="100"
+          :reverse="false"
+          line-cap="round"
+          :fill="fill"
+          empty-fill="rgba(0, 0, 0, .1)"
+          :animation-start-value="0.0"
+          :start-angle="0"
+          insert-mode="append"
+          :thickness="5"
+          :show-percent="true"
+          @vue-circle-progress="progress"
+          @vue-circle-end="progress_end">
+            <p>Play</p>
+        </vue-circle>
       </template>
     </div>
 
@@ -17,6 +33,7 @@
 </template>
 
 <script>
+import VueCircle from 'vue2-circle-progress';
 import GameArena from './GameArena.vue';
 // const CARD_WIDTH = 76;
 // const HAND_WIDTH = CARD_WIDTH * 13;
@@ -26,12 +43,14 @@ export default {
   name: 'Main',
 
   components: {
+    VueCircle,
     GameArena
   },
 
   data() {
     return {
-      gameStart: false
+      gameStart: false,
+      fill: { gradient: ['red', 'green', 'blue'] },
     };
   },
 
@@ -49,6 +68,14 @@ export default {
       this.$nextTick(() => {
         this.gameStart = true;
       });
+    },
+
+    progress(event, progress, stepValue) {
+      console.log(stepValue);
+    },
+    progress_end(event) {
+      console.log('Circle progress end');
+      console.log('event', event);
     }
   },
 
