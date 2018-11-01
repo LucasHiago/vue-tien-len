dbe8b190c0c53e95438b541c8bce844b69fa651a
jasondinh <dinhtq1@gmail.com>
2018-10-31 22:16:48 -0400

    random avatar images

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -17,7 +17,7 @@
       <!-- Slot -->
       <div :style="{width: isAi ? '65px': '100px'}">
         <div :class="{active: isActive}">{{ name }}</div>
-        <div v-html="require('./../assets/avatars/man-1.svg')"></div>
+        <div v-html="require(`./../assets/avatars/${this.avatarImage}.svg`)"></div>
       </div>
     </vue-circle>
   </div>
@@ -25,9 +25,22 @@
 
 <script>
 import VueCircle from 'vue2-circle-progress'
+import { random } from 'lodash'
 
 // const inActiveColor = 'rgba(0, 0, 0, .1)'
 const activeColor = '#2ecc71'
+
+const avatarMap = {
+  1: 'man-1',
+  2: 'man-2',
+  3: 'man-3',
+  4: 'man-4',
+  5: 'man',
+  6: 'girl',
+  7: 'girl-1',
+  8: 'boy',
+  9: 'boy-1'
+}
 
 export default {
   name: 'Avatar',
@@ -45,7 +58,7 @@ export default {
       required: true
     },
     avatarSize: {
-      type: Boolean,
+      type: Number,
       default () {
         return 150
       }
@@ -54,6 +67,12 @@ export default {
       type: Boolean,
       default () {
         return true
+      }
+    },
+    avatarImage: {
+      type: String,
+      default () {
+        return avatarMap[random(1, 9)]
       }
     }
   },
